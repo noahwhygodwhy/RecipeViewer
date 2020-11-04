f4565e7acf478c8ebe224d6a5582381d8e8c73fc
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2020-11-04 16:22:02 -0500

    organized views.py

--- RecipeViewer/views.py
+++ RecipeViewer/views.py
@@ -31,138 +31,96 @@ WEIGHT_UNITS = {
 }
 
 
+#################################################################
+#VIEWS
+#################################################################
 
-
-
-# def getRandomDates(count):
-#     dates = list()
-#     for i in range(count):
-#         year = random.randint(2010, 2020)
-#         month = random.randint(1, 12)
-#         day = random.randint(1, 28)
-#         hour = random.randint(0, 23)
-#         minute = random.randint(0, 59)
-#         second = random.randint(0, 59)
-#         t = timezone.make_aware(timezone.datetime(year, month, day, hour, minute, second))
-#         dates.append(t)
-#     return dates
-
-# @csrf_exempt
-# def generateMakesView(request, data={}):
-#     data["good"] = "False"
-#     print("generateMakesView")
-#     try:
-#         if request.method == "POST":
-#             print("it's a post")
-#             howMany = int(request.POST["makeCount"])
-#             for k in range(int(howMany/100)+1):
-#                 print("doing the", k, "th howmany")
-#                 randRecipes = list()
-#                 count = Recipes.objects.aggregate(count=Count("recipe_id"))["count"]
-#                 for x in range(100):
-#                     print("getting recipe", x)
-#                     randomIndex = random.randint(0, count-1)
-#                     randRecipes.append(Recipes.objects.all()[randomIndex])
-                
-#                 randUsers = list()
-#                 count = Users.objects.aggregate(count=Count("user_id"))["count"]
-#                 for x in range(100):
-#                     print("getting user",x)
-#                     randomIndex = random.randint(0, count-1)
-#                     randUsers.append(Users.objects.all()[randomIndex])
-
-#                 dates = getRandomDates(count)
-
-#                 for i in range(100):
-#                     print("i:",i)
-#                     m = Makes(
-#                         meal_id=uuid.uuid4(),
-#                         datetime=dates[i],
-#                         user = randUsers[i],
-#                         recipe=randRecipes[i])
-#                     m.save()
-                    
-#             data["good"] = "True"
-#     except Exception as e:
-#         print(e)
-#         data["good"] = "False"
-#     data["nested"] = "generateMakesView.html"
-#     return render(request, "base.html", data)
-
-
-
-# returns a rendered ingerdientRow for new recipe generation
-def getIngredientRow(request, data={}):    
-    number = request.GET["rowIndex"]
-    data["rowIndex"] = number
-    return render(request, "ingredientRow.html", data)
-    
-
-#handles the POST to Make a recipe (not a new recipe).
 @csrf_exempt
-def makeRecipe(request, data={}):
-    ings = request.POST.getlist("ingredients[][]")
-    try:
-        for x in ings:
-            spl = x.split(",")
-            iid = spl[0]
-            ("iid:" + iid)
-            quant = float(spl[1])
-            ingred = Ingredients.objects.get(ingredient_id = iid)
-            ingred.quantity -= quant
-            ingred.save()
+def addIngredient(request, data={}):
+    if request.method == "POST":
+        name = request.POST["name"]
+        quantity = request.POST["quantity"]
+        unit = request.POST["unit"]
+        location = request.POST["location"].lower()
+        nameGood=False
         try:
-            user=Users.objects.get(username = request.POST["username"])
+            Ingredients.objects.get(name = name)
         except ObjectDoesNotExist as e:
-            user = Users(username=request.POST["username"], user_id = uuid.uuid4())
-            user.save()
-        
-        date = request.POST["date"].split("-")
-        year = int(date[0])
-        month = int(date[1])
-        day = int(date[2])
-        time = request.POST["time"].split(":")
-        hour = int(time[0])
-        minute = int(time[1])
-        theDateTime = timezone.datetime(year, month,day, hour, minute)
-        theDateTime = timezone.make_aware(theDateTime)
+            nameGood=True
+            print(e)
+        if nameGood:
+            x = Ingredients(name=name, quantity=quantity, unit=unit, location=location, ingredient_id = uuid.uuid4())
+            x.save()
+            data["message"]= ("green", "Ingredient Created Successfully")
+        else:
+            data["message"]= ("red", "Ingredient Already Exists. Please choose a different name.")
 
-        m = Makes(meal_id = uuid.uuid4(),
-                user=user,
-                recipe=Recipes.objects.get(recipe_id = request.POST["recipe_id"]),
-                datetime = theDateTime)
-        m.save()
-        return HttpResponse(True)
-    except Exception as e:
-        print("exception")
-        print(e)
-        return HttpResponse(False)
+    data["usedNames"] = Ingredients.objects.values_list('name', flat=True)
+    data["locationNames"] = Ingredients.objects.values_list('location', flat=True).distinct()
 
-#returns whether or not an ingredient exists, pretty self explanetory 
-def getIngredientExists(request, data={}):
-    result = {}
-    exists = Ingredients.objects.filter(name=request.GET["ingName"]).exists()
-    unit = ""
-    location=""
-    if(exists):
-        unit = Ingredients.objects.get(name=request.GET["ingName"]).unit
-        location = Ingredients.objects.get(name=request.GET["ingName"]).location
-        print("loc:",location)
-    return JsonResponse({"exists":exists, "unit":unit, "location":location})
+    data["nested"] = "addIngredient.html"
+    return render(request, "base.html", data)
 
+def recipeDetails(request, data={}):
+    recipe_id = request.GET["recipe_id"]
+    data["recipe"] = Recipes.objects.get(recipe_id=recipe_id)
+    data["recipe"].title = data["recipe"].title.title()
+    ingredients = Usedby.objects.filter(recipe=recipe_id)
+    pantry = list()
+    for x in ingredients:
+        pantry.append(x.ingredient)
+    data["ingredients"] = zip(ingredients, pantry)
+    data["instructions"] = data["recipe"].instructions.split('"')[1::2]
 
-#duh
-def getIngredientQuantity(request, data={}):
-    try:
+    data["usernames"] = Users.objects.values_list('username', flat=True)
+
+    return render(request, "recipeModal.html", data)
+
+def makeView(request, data={}):
+    
+    data["title"] = "All Makes"
+    if request.GET.__contains__("user"):
+        user_id = request.GET["user"]
+        username = Users.objects.get(user_id=user_id).username
+        data["title"] = "All Makes by " + username
+    elif request.GET.__contains__("recipe"):
+        recipe_id = request.GET["recipe"]
+        title = Recipes.objects.get(recipe_id=recipe_id).title
+        data["title"] = "All Makes of " + title
+    
+    data["nested"] = "makes.html"
+    return render(request, "base.html", data)
+
+def recipeView(request, data={}):
+    if request.GET.__contains__("ingredient_id"):
         iid = request.GET["ingredient_id"]
-        quant = Ingredients.objects.get(ingredient_id = iid).quantity
-    except:
-        quant = -1
-        
-    return HttpResponse(quant)
+        data["title"] = "All Recipes That Use " + Ingredients.objects.get(ingredient_id = iid).name.title()
+    elif request.GET.__contains__("author"):
+        user_id = request.GET["author"]
+        data["title"] = "Recipes by " + Users.objects.get(user_id=user_id).username
+    elif request.GET.__contains__("view"):
+        if request.GET["view"] == "wcim":
+            data["title"] = "What can I make?"
+    else:
+        data["title"] = "All Recipes"
+        data["recipes"] = Recipes.objects.all()
+    
+    data["nested"] = "recipes.html"
+    return render(request, "base.html", data)
 
+def ingredientView(request):
+    data={}
+    data["nested"] = "ingredients.html"
+    return render(request, "base.html", data)
 
-#this is the one that handles posts for actually making a new recipe, not "Make"ing an existing reicpe.
+def analyticsView(request, data={}):
+    data["nested"] = "analytics.html"
+    return render(request, 'base.html', data)
+
+#################################################################
+#OTHER HTML RENDERING STUFF
+#################################################################
+
 @csrf_exempt
 def addRecipe(request, data={}):
     data["message"] = [True, ""]
@@ -220,88 +178,97 @@ def addRecipe(request, data={}):
         data["nested"] = "addRecipe.html"
         return render(request, "base.html", data)
 
-
-
-#This handles making of new ingredients
-@csrf_exempt
-def addIngredient(request, data={}):
-    if request.method == "POST":
-        name = request.POST["name"]
-        quantity = request.POST["quantity"]
-        unit = request.POST["unit"]
-        location = request.POST["location"].lower()
-        nameGood=False
-        try:
-            Ingredients.objects.get(name = name)
-        except ObjectDoesNotExist as e:
-            nameGood=True
-            print(e)
-        if nameGood:
-            x = Ingredients(name=name, quantity=quantity, unit=unit, location=location, ingredient_id = uuid.uuid4())
-            x.save()
-            data["message"]= ("green", "Ingredient Created Successfully")
-        else:
-            data["message"]= ("red", "Ingredient Already Exists. Please choose a different name.")
-
-    data["usedNames"] = Ingredients.objects.values_list('name', flat=True)
-    data["locationNames"] = Ingredients.objects.values_list('location', flat=True).distinct()
-
-    data["nested"] = "addIngredient.html"
-    return render(request, "base.html", data)
-
-def updateIngredientQuantity(request, data={}):
-    ig = Ingredients.objects.get(ingredient_id = request.GET["ingredient_id"])
-    ig.quantity=request.GET["newValue"]
-    ig.save()
-    return HttpResponse()
+def getIngredientRow(request, data={}):    
+    number = request.GET["rowIndex"]
+    data["rowIndex"] = number
+    return render(request, "ingredientRow.html", data)
 
 def ingredientDetails(request, data={}):
     ingredient_id = request.GET["ingredient_id"].split("_")[1]
     data["ingredient"] = Ingredients.objects.get(ingredient_id = ingredient_id)
     return render(request, "ingredientModal.html", data)
 
-def recipeDetails(request, data={}):
+def getTopFiveIngByQuant(request, data={}):
+    labels = []
+    chartData = []
 
-    recipe_id = request.GET["recipe_id"]
-    data["recipe"] = Recipes.objects.get(recipe_id=recipe_id)
-    data["recipe"].title = data["recipe"].title.title()
-    ingredients = Usedby.objects.filter(recipe=recipe_id)
-    pantry = list()
-    for x in ingredients:
-        pantry.append(x.ingredient)
-    data["ingredients"] = zip(ingredients, pantry)
-    data["instructions"] = data["recipe"].instructions.split('"')[1::2]
+    queryset = Ingredients.objects.order_by('-quantity')[:5]
+    for ingredient in queryset:
+        labels.append(ingredient.name)
+        chartData.append(ingredient.quantity)
+    data["labels"] = labels
+    data["data"] = chartData
+    data["chartID"] = uuid.uuid4()
+    return render(request, 'pieChart.html', data)
 
-    data["usernames"] = Users.objects.values_list('username', flat=True)
+#################################################################
+#OTHER
+#################################################################
 
-    return render(request, "recipeModal.html", data)
+@csrf_exempt
+def makeRecipe(request, data={}):
+    ings = request.POST.getlist("ingredients[][]")
+    try:
+        for x in ings:
+            spl = x.split(",")
+            iid = spl[0]
+            ("iid:" + iid)
+            quant = float(spl[1])
+            ingred = Ingredients.objects.get(ingredient_id = iid)
+            ingred.quantity -= quant
+            ingred.save()
+        try:
+            user=Users.objects.get(username = request.POST["username"])
+        except ObjectDoesNotExist as e:
+            user = Users(username=request.POST["username"], user_id = uuid.uuid4())
+            user.save()
+        
+        date = request.POST["date"].split("-")
+        year = int(date[0])
+        month = int(date[1])
+        day = int(date[2])
+        time = request.POST["time"].split(":")
+        hour = int(time[0])
+        minute = int(time[1])
+        theDateTime = timezone.datetime(year, month,day, hour, minute)
+        theDateTime = timezone.make_aware(theDateTime)
 
-def userView(request, data={}):
-    users = Users.objects.all()
-    threeData = list()
-    for u in users:
-        recipeCount = Recipes.objects.filter(user = u).count()
-        makesCount = Makes.objects.filter(user = u).count()
-        threeData.append({"recipeCount":recipeCount, "makesCount":makesCount, "user":u});
+        m = Makes(meal_id = uuid.uuid4(),
+                user=user,
+                recipe=Recipes.objects.get(recipe_id = request.POST["recipe_id"]),
+                datetime = theDateTime)
+        m.save()
+        return HttpResponse(True)
+    except Exception as e:
+        print("exception")
+        print(e)
+        return HttpResponse(False)
 
-    data["threeData"] = threeData
-    data["nested"] = "users.html"
-    return render(request, "base.html", data)
+def getIngredientExists(request, data={}):
+    result = {}
+    exists = Ingredients.objects.filter(name=request.GET["ingName"]).exists()
+    unit = ""
+    location=""
+    if(exists):
+        unit = Ingredients.objects.get(name=request.GET["ingName"]).unit
+        location = Ingredients.objects.get(name=request.GET["ingName"]).location
+        print("loc:",location)
+    return JsonResponse({"exists":exists, "unit":unit, "location":location})
 
-def makeView(request, data={}):
+def getIngredientQuantity(request, data={}):
+    try:
+        iid = request.GET["ingredient_id"]
+        quant = Ingredients.objects.get(ingredient_id = iid).quantity
+    except:
+        quant = -1
+        
+    return HttpResponse(quant)
 
-    data["title"] = "All Makes"
-    if request.GET.__contains__("user"):
-        user_id = request.GET["user"]
-        username = Users.objects.get(user_id=user_id).username
-        data["title"] = "All Makes by " + username
-    elif request.GET.__contains__("recipe"):
-        recipe_id = request.GET["recipe"]
-        title = Recipes.objects.get(recipe_id=recipe_id).title
-        data["title"] = "All Makes of " + title
-    
-    data["nested"] = "makes.html"
-    return render(request, "base.html", data)
+def updateIngredientQuantity(request, data={}):
+    ig = Ingredients.objects.get(ingredient_id = request.GET["ingredient_id"])
+    ig.quantity=request.GET["newValue"]
+    ig.save()
+    return HttpResponse()
 
 def theresEnough(ub, ing):
     have = ing.quantity
@@ -318,29 +285,65 @@ def theresEnough(ub, ing):
         have = have*WEIGHT_UNITS[haveUnit]/WEIGHT_UNITS[wantedUnit];
     return have>=wanted
 
+def getMakesPerUserGraph(request, data={}):
+    print("getMakesPerUser")
+    labels = list()
+    chartData = list()
+    users = dict()
+    user_ids = Makes.objects.values("user_id").annotate(entries=Count("user_id"))
 
-def recipeView(request, data={}):
-    if request.GET.__contains__("ingredient_id"):
-        iid = request.GET["ingredient_id"]
-        data["title"] = "All Recipes That Use " + Ingredients.objects.get(ingredient_id = iid).name.title()
-    elif request.GET.__contains__("author"):
-        user_id = request.GET["author"]
-        data["title"] = "Recipes by " + Users.objects.get(user_id=user_id).username
-    elif request.GET.__contains__("view"):
-        if request.GET["view"] == "wcim":
-            data["title"] = "What can I make?"
-    else:
-        data["title"] = "All Recipes"
-        data["recipes"] = Recipes.objects.all()
+    counts = dict()
+    for x in user_ids:
+        makes = x["entries"]
+        if makes in counts:
+            counts[makes] += 1
+        else:
+            counts[makes] = 1   
+    dataTobeSorted = list()
+    for x in counts:
+        dataTobeSorted.append((x, counts[x]))
+    dataTobeSorted = sorted(dataTobeSorted)
+    mx = max(dataTobeSorted, key=lambda x:x[1])
     
-    data["nested"] = "recipes.html"
-    return render(request, "base.html", data)
+    colors = "["
+    for x in dataTobeSorted:
+        labels.append(x[0])
+        chartData.append(x[1])
+        red = 250-x[0]*2
+        blue = 0+x[0]*4
+        green= 0
+        colors += '"#' + f'{red:02x}' + f'{green:02x}' + f'{blue:02x}' + '",'
+    colors.strip(",")
+    colors += "]"
 
-def ingredientView(request):
-    data={}
-    data["nested"] = "ingredients.html"
-    return render(request, "base.html", data)
+    data["backgroundColor"] = colors
+    data["labels"] = labels
+    data["data"] = chartData
+    data["chartID"] = uuid.uuid4()
+    return render(request, 'barGraph.html', data)
 
+    #counts is now a dict of Makes:How mnay people have that many makes
+
+
+    #todo:
+
+#top five recipes by review count
+def getTFRBRC(request, data={}):
+    labels = []
+    chartData = []
+
+    queryset = Recipes.objects.order_by('-review_count')[:5]
+    for recipe in queryset:
+        labels.append(recipe.title)
+        chartData.append(recipe.review_count)
+    data["labels"] = labels
+    data["data"] = chartData
+    data["chartID"] = uuid.uuid4()
+    return render(request, 'pieChart.html', data)
+    
+#################################################################
+#DATATABLE VIEWS
+#################################################################
 class makesViewData(BaseDatatableView):
     columns = ["user", "recipe", "datetime", "user_id, ""recipe_id"]
     order_columns = ["user", "recipe", "datetime"]
@@ -435,77 +438,5 @@ class recipeViewData(BaseDatatableView):
             qs = qs.filter(recipe_id__in=actualList)
         return qs
 
-def analyticsView(request, data={}):
-    data["nested"] = "analytics.html"
-    return render(request, 'base.html', data)
 
 
-
-def getTopFiveIngByQuant(request, data={}):
-    labels = []
-    chartData = []
-
-    queryset = Ingredients.objects.order_by('-quantity')[:5]
-    for ingredient in queryset:
-        labels.append(ingredient.name)
-        chartData.append(ingredient.quantity)
-    data["labels"] = labels
-    data["data"] = chartData
-    data["chartID"] = uuid.uuid4()
-    return render(request, 'pieChart.html', data)
-
-def getMakesPerUserGraph(request, data={}):
-    print("getMakesPerUser")
-    labels = list()
-    chartData = list()
-    users = dict()
-    user_ids = Makes.objects.values("user_id").annotate(entries=Count("user_id"))
-
-    counts = dict()
-    for x in user_ids:
-        makes = x["entries"]
-        if makes in counts:
-            counts[makes] += 1
-        else:
-            counts[makes] = 1   
-    dataTobeSorted = list()
-    for x in counts:
-        dataTobeSorted.append((x, counts[x]))
-    dataTobeSorted = sorted(dataTobeSorted)
-    mx = max(dataTobeSorted, key=lambda x:x[1])
-    
-    colors = "["
-    for x in dataTobeSorted:
-        labels.append(x[0])
-        chartData.append(x[1])
-        red = 250-x[0]*2
-        blue = 0+x[0]*4
-        green= 0
-        colors += '"#' + f'{red:02x}' + f'{green:02x}' + f'{blue:02x}' + '",'
-    colors.strip(",")
-    colors += "]"
-
-    data["backgroundColor"] = colors
-    data["labels"] = labels
-    data["data"] = chartData
-    data["chartID"] = uuid.uuid4()
-    return render(request, 'barGraph.html', data)
-
-    #counts is now a dict of Makes:How mnay people have that many makes
-
-
-    #todo:
-
-#top five recipes by review count
-def getTFRBRC(request, data={}):
-    labels = []
-    chartData = []
-
-    queryset = Recipes.objects.order_by('-review_count')[:5]
-    for recipe in queryset:
-        labels.append(recipe.title)
-        chartData.append(recipe.review_count)
-    data["labels"] = labels
-    data["data"] = chartData
-    data["chartID"] = uuid.uuid4()
-    return render(request, 'pieChart.html', data)
